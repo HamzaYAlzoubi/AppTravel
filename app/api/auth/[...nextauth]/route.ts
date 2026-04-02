3321d1ca299accc42c0336ee45d0dcc15f077b0b
hamza alzoubi <hamzaalzoubi94@gmail.com>
2026-04-01 19:38:09 -0700

    7

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import { authOptions } from "@/lib/auth/auth-options";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+// import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
